85325611b506867e9c509837e28184652b6611cb
Julien Beisel <julienbeisel@gmail.com>
2019-01-08 15:49:32 +0100

    yoyoyoyoyoyo

--- dictionnaire_exemple.py
+++ dictionnaire_exemple.py
@@ -3,7 +3,7 @@ dict_peak = {'d': {'>': {"next_state" :'d',"semantic_letter" : "out"},
         '<': {"next_state" :'r',"semantic_letter" : "out"},"isBeginning" : False},
         'r': {'>': {"next_state" :'t',"semantic_letter" : "found"},
         '=': {"next_state" :'r',"semantic_letter" : "maybe_before"},
-        '<': {"next_state" :'t',"semantic_letter" : "maybe_before"},"isBeginning" : },
+        '<': {"next_state" :'t',"semantic_letter" : "maybe_before"},"isBeginning" : False},
         't': {'>': {"next_state" :'t',"semantic_letter" : "in"},
         '=': {"next_state" :'t',"semantic_letter" : "maybe_after"},
-        '<': {"next_state" :'r',"semantic_letter" : "out_after"}}}
+        '<': {"next_state" :'r',"semantic_letter" : "out_after"},"isBeginning" : False}}
